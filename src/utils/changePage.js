ce9eb8cd469da308ae39543542c808b2ba89c9c9
Stephen Suresh <stephen.suresh@outlook.com>
2023-07-27 17:15:28 -0400

    'page' switching functionality that changes html in mainContent div

--- src/utils/changePage.js
+++ src/utils/changePage.js
@@ -1,5 +1,14 @@
+import menu from "../components/menu/menu";
+import home from "../components/home/home";
+import contact from "../components/contact/contact";
 
 export default function changePage(evt) {
-    const page = evt.target.id;
-    console.log(page);
+    // https://stackoverflow.com/questions/52447250/calling-an-exported-function-by-a-string-value-of-the-function-name
+    const pages = { menu, home, contact };
+    const selection = evt.target.id;
+    const content = pages[selection]();
+    
+    const main = document.querySelector('#main');
+    main.innerHTML = "";
+    main.appendChild(content);
 }
