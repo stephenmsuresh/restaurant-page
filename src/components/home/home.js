6e90a7b81c2bffc7d5ee9773fad0bca50aed68be
Stephen Suresh <stephen.suresh@outlook.com>
2023-07-28 01:40:20 -0400

    add restaurant times to home page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -9,10 +9,14 @@ export default function home() {
     blurb.innerText = "Step into a world of vibrant retro vibes and delectable treats at our whimsical ice cream parlor. Indulge your taste buds with our handcrafted, creamy concoctions available in an array of dazzling flavors, each one carefully curated to satisfy your sweetest cravings. Get ready to groove to the infectious tunes of the disco as you savor the delightful experience of Disco Ice Cream, the ultimate destination for a delicious journey through nostalgia!";
 
     const address = document.createElement('p');
-    address.innerHTML = "123 Ice Street, Toronto, ON";
+    address.innerText = "123 Ice Street, Toronto, ON";
+
+    const time = document.createElement('p');
+    time.innerText = "Mondays to Saturdays, 10:00 A.M. to 7:00 P.M"
 
     content.appendChild(header);
     content.appendChild(blurb);
     content.appendChild(address);
+    content.appendChild(time);
     return content;
 }
