77cba319158277aa06151b6affd8fb672dbb78d5
Stephen Suresh <stephen.suresh@outlook.com>
2023-07-28 00:03:25 -0400

    create contact page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,3 +1,19 @@
-export default function contact(){
-    console.log("in contact");
+export default function contact() {
+    const content = document.createElement('div');
+    content.setAttribute('id', 'contact-page');
+
+    const header = document.createElement('h1');
+    header.textContent = "Contact Us"
+
+    const email = document.createElement('p');
+    email.innerText = 'discoicecream@disco.com';
+
+    const phone = document.createElement('p');
+    phone.innerText = '123-456-7890';
+
+    content.appendChild(header);
+    content.appendChild(email);
+    content.appendChild(phone);
+
+    return content;
 }
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,10 +4,15 @@ export default function home() {
 
     const header = document.createElement('h1');
     header.innerText = "Welcome to Disco Ice Cream - Where Flavor Meets Funk!"
-    content.appendChild(header);
 
     const blurb = document.createElement('p');
     blurb.innerText = "Step into a world of vibrant retro vibes and delectable treats at our whimsical ice cream parlor. Indulge your taste buds with our handcrafted, creamy concoctions available in an array of dazzling flavors, each one carefully curated to satisfy your sweetest cravings. Get ready to groove to the infectious tunes of the '70s as you savor the delightful experience of Disco Ice Cream, the ultimate destination for a delicious journey through nostalgia!";
+
+    const address = document.createElement('p');
+    address.innerHTML = "123 Ice Street, Toronto, ON";
+
+    content.appendChild(header);
     content.appendChild(blurb);
+    content.appendChild(address);
     return content;
 }
